98c046d53f4bb37f6262a4dfe47721369e863d1c
physicsvaib <physicsvaib@gmail.com>
2026-03-20 10:19:38 +0530

    Pydantic models

--- app/base.py
+++ app/base.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI, status, HTTPException
 from scalar_fastapi import get_scalar_api_reference
-from typing import Any
+from typing import Any, List
+from .schemas import Shipment, ShipmentStatus
 
 gap = FastAPI()
 
+
 shipment_dict = {
-    123: {"content": "cargo", "status": "transit", "weight": 20.2},
+    123: {"content": "cargo", "status": "in_transit", "weight": 20.2},
     124: {"content": "cargo", "status": "placed", "weight": 1.2},
-    125: {"content": "cargo", "status": "transit", "weight": 21.52},
+    125: {"content": "cargo", "status": "in_transit", "weight": 21.52},
     126: {"content": "food", "status": "placed", "weight": 4.2},
-    127: {"content": "utils", "status": "done", "weight": 10.2},
+    127: {"content": "utils", "status": "delivered", "weight": 10.2},
 }
 
 
 @gap.get("/scalar", include_in_schema=False)
 def get_scalar_api():
-    obj = get_scalar_api_reference(
-        title="My Custom API Handler", openapi_url=gap.openapi_url
-    )
+    obj = get_scalar_api_reference(openapi_url=gap.openapi_url)
     return obj
 
 
-@gap.get("/shipment")
-def get_shipment(id: int) -> dict[str, str]:
+@gap.get("/shipment", response_model=Shipment)
+def get_shipment(id: int):
     if id in shipment_dict:
-        return {"status": str(shipment_dict[id])}
+        return shipment_dict[id]
     else:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND, detail="Given Id Dont exist can we look again"
@@ -32,18 +32,14 @@ def get_shipment(id: int) -> dict[str, str]:
 
 
 @gap.post("/shipment")
-def post_shipment(data: dict[str, Any]) -> dict[str, int]:
-
-    if not (data["content"] and data["weight"]):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="check your inputs"
-        )
-
-    content = data["content"]
-    weight = data["weight"]
+def post_shipment(data: Shipment) -> dict[str, int]:
 
     id = max(shipment_dict.keys()) + 1
-    shipment_dict[id] = {"content": content, "status": "placed", "weight": weight}
+    shipment_dict[id] = {
+        "content": data.content,
+        "status": "placed",
+        "weight": data.weight,
+    }
 
     return {"id": id}
 
@@ -54,7 +50,7 @@ def get_shipment_field(field: str, id: int) -> dict[str, Any]:
 
 
 @gap.put("/shipment")
-def update_shipment_status(id: int, del_status: str):
+def update_shipment_status(id: int, del_status: ShipmentStatus):
     if id not in shipment_dict:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="no id found")
     shipment_dict[id]["status"] = del_status
@@ -71,3 +67,8 @@ def delete_shipment(id: int) -> dict[str, str]:
             status.HTTP_404_NOT_FOUND,
             detail="The thing you are looking for is not here",
         )
+
+
+@gap.get("/all_shipments")
+def get_all_shipments() -> dict[str, List[int]]:
+    return {"keys": [i for i in shipment_dict.keys()]}
---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class ShipmentStatus(str, Enum):
+    booked = "booked"
+    in_transit = "in_transit"
+    delivered = "delivered"
+    placed = "placed"
+
+
+class Shipment(BaseModel):
+    content: str = Field(description="What is the content here")
+    weight: float = Field(
+        lt=250, description="Expected weight here, a bit fuzzy are we"
+    )
+    status: ShipmentStatus = Field(default=ShipmentStatus.booked)
